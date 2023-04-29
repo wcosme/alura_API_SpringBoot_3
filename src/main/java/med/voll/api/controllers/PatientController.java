e095cabda94cc1b8cdb3a6236c102545308e41a9
Wallace Gonçalves <wallacecosme@gmail.com>
2023-04-29 15:34:35 -0300

    Ajustes

--- src/main/java/med/voll/api/controllers/PatientController.java
+++ src/main/java/med/voll/api/controllers/PatientController.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/vi/patients")
+@RequestMapping("api/v1/patients")
 public class PatientController {
 
     @Autowired
