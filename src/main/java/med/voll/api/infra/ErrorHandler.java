05fd93f2bd37aa614a524ae63bc299af56960de3
Wallace Gonçalves <wallacecosme@gmail.com>
2023-05-12 19:28:58 -0300

    Tratando exception 400

--- src/main/java/med/voll/api/infra/ErrorHandler.java
+++ src/main/java/med/voll/api/infra/ErrorHandler.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +15,22 @@ public class ErrorHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity error404(){
+
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400(MethodArgumentNotValidException ex){
+
+        var error = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(error.stream().map(DatailErrorValidation::new).toList());
+    }
+
+    private record DatailErrorValidation(String campo, String message){
+
+        public DatailErrorValidation(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
